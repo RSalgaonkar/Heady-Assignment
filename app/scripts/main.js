7f34332566b837238a335261086a7d27d9e805de
Rashmith <salgaonkaar.rashmith.com>
2018-01-31 19:01:40 +0530

    footer fixes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,24 +12,13 @@ $('.select-id').mouseover(function() {
 });
 
 $('#details-img').hide().slideDown(2000);
-$(".description-img").hide().slideDown(2000);
+$('.description-img').hide().slideDown(2000);
 $('.second-descp').hide().slideDown(2000);
 
-// $('.over-text').animate({ 
-//     top: 100,
-//     left: 100,
-// }).animate({
-//     top: 0,
-//     left: 0,
-// })
-
-// $('.description-img').animate({ 
-//     top: 600,
-// }).animate({
-//     top: 0,
-// })
-
-// $(".description-img").fadeIn()
-// 	.css({top:1000,position:'absolute'})
-// 	.animate({top: 0}, 800, function() {
-// });
+$('.over-text').animate({ 
+    top: 100,
+    left: 100,
+}).animate({
+    top: 0,
+    left: 0,
+})
