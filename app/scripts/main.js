a7d497d7b7c406e34b852f9ddf662254e85c6adb
Rashmith <salgaonkaar.rashmith.com>
2018-01-31 17:38:07 +0530

    minor changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,3 +10,26 @@ if( $('#details').length ) {
 $('.select-id').mouseover(function() {
   $('#selected-section').html(this.id);
 });
+
+$('#details-img').hide().fadeIn(2000);
+
+
+// $('.over-text').animate({ 
+//     top: 100,
+//     left: 100,
+// }).animate({
+//     top: 0,
+//     left: 0,
+// })
+
+// $('.description-img').animate({ 
+//     top: 600,
+// }).animate({
+//     top: 0,
+// })
+
+// $(".description-img").fadeIn()
+// 	.css({top:1000,position:'absolute'})
+// 	.animate({top: 0}, 800, function() {
+    
+// });
