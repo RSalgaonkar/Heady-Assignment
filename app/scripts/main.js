33acd882b80b25a195fff553458e53b07e629383
Rashmith <salgaonkaar.rashmith.com>
2018-01-31 15:21:56 +0530

    select id on hovering

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,3 +2,11 @@ $('.select-item').on('click', function(event) {
 	var value = this.attributes['name'].value;
 	$('#selected-section').html(value);
 })
+
+if( $('#details').length ) {
+  $('#selected-section').html('details');
+}
+
+$('.select-id').mouseover(function() {
+  $('#selected-section').html(this.id);
+});
